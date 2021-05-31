056840eb44839f66fdccf630648696c2345421eb
asas1313 <Sriuba11>
2021-05-31 16:49:33 +0300

    password change and reset implemented

--- lib/app/views/profile_view.dart
+++ lib/app/views/profile_view.dart
@@ -2,64 +2,114 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:inkubox_app/app/controllers/profile_controller.dart';
 import 'package:inkubox_app/app/views/widgets/authenticated.dart';
-import 'package:inkubox_app/app/views/widgets/avatar.dart';
 import 'package:responsive_builder/responsive_builder.dart';
+
+import 'widgets/avatar.dart';
 
 class ProfileView extends GetWidget<ProfileController> {
   @override
   Widget build(BuildContext context) {
     return Authenticated(
-      child: FittedBox(
-          fit: BoxFit.contain,
+      child: SingleChildScrollView(
           child: ResponsiveBuilder(builder: (context, sizingInformation) {
-            var width =
-                (sizingInformation.isDesktop) ? Get.width / 3 : Get.width;
-            return Container(
-              width: width,
-              height: 600,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 15),
-                    Avatar(
-                      avatarUrl: controller.avatarUrl.value,
-                      onTap: () => controller.setAvatar(),
-                    ),
-                    SizedBox(height: 15),
-                    TextFormField(
-                      initialValue: controller.email,
-                      enabled: false,
-                      decoration: InputDecoration(
-                          labelText: 'Email',
-                          hintStyle: TextStyle(fontWeight: FontWeight.w600)),
-                    ),
-                    SizedBox(height: 15),
-                    TextFormField(
-                      controller: controller.displayName,
-                      decoration: InputDecoration(
-                          labelText: 'Display name',
-                          hintStyle: TextStyle(fontWeight: FontWeight.w600)),
-                    ),
-                    SizedBox(height: 15),
-                    TextFormField(
-                      controller: controller.position,
-                      decoration: InputDecoration(
-                          labelText: 'Position',
-                          hintStyle: TextStyle(fontWeight: FontWeight.w600)),
-                    ),
-                    SizedBox(height: 15),
-                    TextFormField(
-                      controller: controller.phone,
-                      decoration: InputDecoration(
-                          labelText: 'Phone',
-                          hintStyle: TextStyle(fontWeight: FontWeight.w600)),
-                    ),
-                  ],
+        var width = (sizingInformation.isDesktop) ? Get.width / 3 : Get.width;
+        return Container(
+          width: width,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: 15),
+                Avatar(
+                  avatarUrl: controller.avatarUrl.value,
+                  onTap: () => controller.setAvatar(),
                 ),
-              ),
-            );
-          })),
+                SizedBox(height: 15),
+                TextFormField(
+                  initialValue: controller.email,
+                  enabled: false,
+                  decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintStyle: TextStyle(fontWeight: FontWeight.w600)),
+                ),
+                SizedBox(height: 15),
+                TextFormField(
+                  controller: controller.displayName,
+                  decoration: InputDecoration(
+                      labelText: 'Display name',
+                      hintStyle: TextStyle(fontWeight: FontWeight.w600)),
+                ),
+                SizedBox(height: 15),
+                TextFormField(
+                  controller: controller.position,
+                  decoration: InputDecoration(
+                      labelText: 'Position',
+                      hintStyle: TextStyle(fontWeight: FontWeight.w600)),
+                ),
+                SizedBox(height: 15),
+                TextFormField(
+                  controller: controller.phone,
+                  decoration: InputDecoration(
+                      labelText: 'Phone',
+                      hintStyle: TextStyle(fontWeight: FontWeight.w600)),
+                ),
+                SizedBox(height: 25),
+                TextFormField(
+                  controller: controller.oldPassword,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'Password'),
+                  autovalidateMode: AutovalidateMode.always,
+                ),
+                SizedBox(height: 15),
+                TextFormField(
+                  controller: controller.newPassword,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'New password'),
+                  autovalidateMode: AutovalidateMode.always,
+                  validator: (value) {
+                    if (controller.oldPassword.text.isNotEmpty &&
+                        (value ?? '').isEmpty) {
+                      return 'Password is required.';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 15),
+                TextFormField(
+                  controller: controller.confirmPassword,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'Confirm password'),
+                  autovalidateMode: AutovalidateMode.always,
+                  validator: (value) {
+                    if (value != controller.newPassword.text) {
+                      return 'Passwords do not match.';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 15),
+                ElevatedButton(
+                  onPressed: () async {
+                    var _error = await controller.validatePassword();
+                    if (_error == null) {
+                      controller.changePassword();
+                    } else {
+                      Get.snackbar(
+                        'Error',
+                        _error,
+                        backgroundColor: Colors.red,
+                        duration: Duration(seconds: 5),
+                      );
+                    }
+                  },
+                  child: Text('Change password'),
+                ),
+                SizedBox(height: 15),
+              ],
+            ),
+          ),
+        );
+      })),
     );
   }
 }
